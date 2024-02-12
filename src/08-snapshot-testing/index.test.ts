88f3e030bcb45db80f6b375bb44ed0bbbc921710
Andrei Ihnatsiuk <garnich@tut.by>
2024-02-12 12:31:38 +0300

    basic_testing: 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,31 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const expectReult = {
+  next: {
+    next: {
+      next: {
+        next: {
+          next: null,
+          value: null,
+        },
+        value: 'd',
+      },
+      value: 'c',
+    },
+    value: 'b',
+  },
+  value: 'a',
+};
+
+const values: string[] = ['a', 'b', 'c', 'd'];
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(values)).toStrictEqual(expectReult);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(values)).toMatchSnapshot(expectReult);
   });
 });
